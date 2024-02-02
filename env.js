78dad5aa21fe0014327331531400c20403868b6a
Jeffery Kizarmie Adolor <jefferyadolor18@gmail.com>
2024-02-02 05:31:23 +0000

    moved project up to the mian directory
    [cleaned: PolinRider payload removed, real changes kept]

---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+  "type": "service_account",
+  "project_id": "chatbot-399016",
+  "private_key_id": "d2a4309ed650bb0ed64266be175b7eb88a93cfd0",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDTYtXwx4bVm1VX\nVmdYmXAjcvG7Y4XPRXJCLcWeJECr6rlt3EUtkJy45wXLCxovOhlZFa5tCfB+GuBE\nK3aY1CB14UElTECv8pwvPo2DQDsUIwaSaXEwfnKJtLopwWrqcp/m3oKOvwjsTp/s\nlKEhzta5L0Rs3nkaeT40WzaC55a8VjuKtAwbPtPLaTtfyfA4caBTa0nwmFsrMBkm\nciGYvia13WygosunneB926L93IZ8VYDQohCK5FtHLsoZeq4ZZQMqtsVKH+xRIsnl\nLq4nExnADtQmcnrkNJqL7QZjEpLzJ3S2MheTuUMm+2wcz+kVSLm4SrUBP1DfDXRP\nIbBvb8PnAgMBAAECggEAEMYghPVIUU38tSwBkmnGmMGHxh9tIFf56593NYfZ6+p5\n4WpfwPBfR1rpvkV4w/OfzE5oo3Vs9IzMbAxo4lK+6qu/wCZvYabmVudnUll39q6a\n2GZumXMHQr8pIKQdCFKlNqxEfvBQ1HOexCa9LJIdvt8t1lfHE184nO36AuQ+tV4b\negzlagHwCP7dDWeilK/VK4Cy60phikg3cWSYpXo2q2XLBJO2i0xtYkzHQ6qPSBnm\n/R1SgPGEGlEFXUUKCYVUyq9PdisPpK4QTbIB2KdbLFkAHQ2fInKhQ8KDTIhFiR1x\nw7rESscuFXIeW80NoSTafr6lsMwnY/KCBpxwk5rB9QKBgQDsJE/uRzYUOY08pqu4\ntm5cr2Itx8D2+sVP2w7sPaMkGw0KGldx1m1gUVR1hyjDfbncR4SUKcjPw/QjQH3p\nIqyVuEnxuJTCfLrgtp+Z/AxVAjZ9UrfuITnDmj23tJk9CU5j0JTjQ+2MAldgLHuB\naBA9CMMoI4MJw1++K6v3t/HOhQKBgQDlKZQdGDgzOsiSrvGRzBPcRjFOkUjEawIS\nSpLIkA4Uw4BMWsqxUNvMXVoujKQcQhAuUYYPjVYSjEGI1HzqDXglxehNgKIi+bWE\nOyuKq/0ap9Ud5P7Xe7ybglJj65v2eEKDnBGfczDO0I2Cv/Hh63fCoYrTtIQs6jYe\nPuex+HqCewKBgA1pOojjznRzmEvbg2EoKzhkVYUYFSNxtocEf+J21a8VQjZSrKiw\nbuHLfKp4FJ6Vo1qZTTRE8zypelIONZhp+bc/EiQCQaCSCOL+PC44nFKvn3yamAuU\nI32yYtLuFM3cm3EbxXjqwLD06RVzMUh2KoXs23iKG8UQLX3VaRzWe4spAoGBAKSK\nRh7d1PxzqLApjKnyrg/rFlwjVOwzKTmY8euwA2xFM0mfpwnJ1QfXv3jM4sinZpbb\nUja4dKS8sBFfAJHNOgdlIncDdWdHhF19XrYi54jMGOluEs0Fu1atLBsygLlnf9qf\npMeoHdNhSzomN7bPKx5Efzmi04SsZk8xO4s9rjkhAoGBAJ+otNhl7al6EQj9pRt7\nntOkkQ7Gt0DFYUyipn6UxH4VIA9R4gnYKxhqPoMaeZ7ababbOEQwfX/fN1IJrfNM\nB6Pm87R2ZP2Z9gI15k7WGZo1xAVPps/6n2dEjMpqto0TTR9m1tZYrqvfAoXJ3fMo\n/GG5aJbrhO8xVf6NRswcis99\n-----END PRIVATE KEY-----\n",
+  "client_email": "chatbot-project@chatbot-399016.iam.gserviceaccount.com",
+  "client_id": "105700993849970184201",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chatbot-project%40chatbot-399016.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
